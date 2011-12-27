a965261131cf773ed436bf1dbba3c15249453dda
Ruslan Spivak <ruslan.spivak@gmail.com>
2011-12-27 13:52:48 -0500

    Bump version to 0.4

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ def read(*rel_names):
 
 setup(
     name='httpcode',
-    version='0.3',
+    version='0.4',
     url='http://github.com/rspivak/httpcode',
     license='MIT',
     description='httpcode - explain HTTP status code',
